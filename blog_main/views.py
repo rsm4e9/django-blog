c202ee46f4d5661a28be571ab960ab6c829e5760
RS <ravisatyamanikanta333@gmail.com>
2026-07-23 00:14:46 +0530

    Updated Blog Files

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,13 @@
 
 from django .shortcuts import render
 
-
+from blogs.models import Blog, category
 def home(request):
-    return render(request,'home.html')
+    categories = category.objects.all()
+    featured_post = Blog.objects.filter(is_featured=True).order_by('updated_at')
+    print(featured_post)
+    context={
+        'categories':categories,
+        'featured_post':featured_post,
+    }
+    return render(request,'home.html',context)
