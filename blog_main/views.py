3a5f2027ed48e77b4b8888d92650dbb78b6d5141
RS <ravisatyamanikanta333@gmail.com>
2026-09-03 12:10:40 +0530

    blogs and search filter features

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,13 +1,22 @@
 from django .shortcuts import render
 from blogs.models import Blog, Category
+from assignments.models import About
+
 def home(request):
     # categories = Category.objects.all()
     featured_post = Blog.objects.filter(is_featured=True,status='Published').order_by('updated_at')
     posts=Blog.objects.filter(is_featured=False,status='Published')
-   
+   #Fetch about us
+    try:
+
+        about = About.objects.get()
+
+    except:
+        about=None    
     context={
         # 'categories':categories,
         'featured_post':featured_post,
         'posts':posts,
+        'about':about,
     }
     return render(request,'home.html',context)
