bc2bf0106c112c4e141172b736579d8682a45927
RS <ravisatyamanikanta333@gmail.com>
2026-09-07 13:06:42 +0530

    category crud functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -33,4 +33,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+
+    #dashboards
+    path('dashboard/', include('dashboards.urls')),
 ] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
